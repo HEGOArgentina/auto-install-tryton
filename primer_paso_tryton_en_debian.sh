625dfa556657f3e83843363a44c0742d3a749010
Hernán <hernan@hego.com.ar>
2014-10-03 18:12:00 -0300

    Update primer_paso_tryton_en_debian.sh

--- primer_paso_tryton_en_debian.sh
+++ primer_paso_tryton_en_debian.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-su
-git clone https://github.com/HEGOArgentina/auto-install-tryton.git
-cd auto-install-tryton
+echo Instalación integra de Tryton y modulos para implentar en una distribución Debian Wheezy.
+echo Recuerde ingresar las contraseñas apropiadas en los pasos que se soliciten.
+echo Inicia sesión como usuario root, ingresá la contraseña.
+su - root pwd | cd 
 ./install_tryton_and_module_ar.sh
 exit
