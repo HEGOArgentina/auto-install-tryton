3490a409b6a07e3c94725a78e23aa6b9310968c9
Hernán <hernan@hego.com.ar>
2014-10-03 14:36:49 -0300

    Update install_tryton_and_module_ar.sh

--- install_tryton_and_module_ar.sh
+++ install_tryton_and_module_ar.sh
@@ -1,26 +1,12 @@
-#! /bin/bash
-
-cd /etc/apt && wget http://debian.tryton.org/debian/debian.tryton.org-archive.asc && apt-key add debian.tryton.org-archive.asc
-
-echo "deb http://debian.tryton.org/debian/ wheezy-3.2 main" >> /etc/apt/sources.list
-
-cd /etc/apt/sources.list.d && wget http://debian.tryton.org/debian/tryton-wheezy-3.2.list
-
-apt-get update
-apt-get install tryton-server -t wheezy-3.2
-apt-get install tryton-modules-all -t wheezy-3.2
-apt-get install tryton-client -t wheezy-3.2
-cd /etc/apt/preferences.d && wget http://debian.tryton.org/debian/debian.tryton.org.pref
-apt-get update && apt-get dist-upgrade
-###
-###
-apt-get install git
-###
 /etc/init.d/postgresql* restart
+
 cd
+
 su - postgres -c "createuser --createdb --no-createrole --no-superuser \
         --pwprompt tryton"
+
 /etc/init.d/tryton-server restart
+
 su - postgres -c "createdb --encoding=UNICODE --owner=tryton tryton"
 
 cd /etc/
@@ -31,18 +17,20 @@ cd trytond-configuracion && cp trytond.conf /etc/
 /usr/bin/trytond -i all -d tryton
 /usr/bin/trytond -u all -d tryton
 
-/etc/init.d/postgresql* restart
-
-/etc/init.d/tryton-server restart
+############################
+# Instalación del Patch de login
 
 cd /usr/lib/python2.7/dist-packages/tryton/
 git clone https://github.com/HEGOArgentina/patch-login-tryton
 
 cd /usr/lib/python2.7/dist-packages/tryton/patch-login-tryton
-cp -R common.py /usr/share/pyshared/tryton/common/common.py
+cp -R common.py /usr/share/pyshared/tryton/common/common.py 
 cp -R dbcreate.py /usr/share/pyshared/tryton/gui/window/dbcreate.py
 cp -R dblogin.py /usr/share/pyshared/tryton/gui/window/dblogin.py
 /etc/init.d/tryton-server restart
+
+############################
+# Instalación de Módulos para Argentina
 
 cd /usr/lib/python2.7/dist-packages/trytond/modules/
 git clone https://github.com/HEGOArgentina/11-modulos-ar.git
@@ -61,7 +49,12 @@ cp -r /usr/lib/python2.7/dist-packages/trytond/modules/11-modulos-ar/country_ar 
 
 rm -r /usr/lib/python2.7/dist-packages/trytond/modules/11-modulos-ar
 
+############################
+# Reiniciamos Los Servidores de Postgre y Tryton
+
 /etc/init.d/postgresql* restart
 /etc/init.d/tryton-server restart
+
+############################
 
 exit
