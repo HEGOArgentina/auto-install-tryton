f77449c126104fd3acedb6b2ff56db24ef9204fd
Hernán <hernan@hego.com.ar>
2014-10-05 19:14:46 -0300

    Delete primer_paso_tryton_en_debian.sh

--- primer_paso_tryton_en_debian.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo Instalación integra de Tryton y modulos para implentar en una distribución Debian Wheezy.
-echo Recuerde ingresar las contraseñas apropiadas en los pasos que se soliciten.
-echo Inicia sesión como usuario root, ingresá la contraseña.
-su - root pwd | cd 
-./install_tryton_and_module_ar.sh
-exit
